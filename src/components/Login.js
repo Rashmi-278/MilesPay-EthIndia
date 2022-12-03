435748b899a39544c75fbf29ab1a0c9182c4da1a
Anubhav Kumar <anubhav11697@gmail.com>
2022-12-03 23:40:16 +0530

    Authentication integrated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,37 +1,37 @@
-import {useState, useEffect, React } from 'react'
+import { useState, React } from 'react'
 import styled from 'styled-components'
 import { ConnectKitButton } from "connectkit";
 import { useAccount } from 'wagmi';
 
 function Login() {
+
   const [Connected, setConnected] = useState(false);
   const { address } = useAccount();
 
     return (
         <Container>
-          <div className="address-div">
-            <p className="address">{address}</p>
+          <div className='address-div'>
+            <p className='address'>{address}</p>
           </div>
-          <div className="button-div">
+          <div className='login-button'>
           <ConnectKitButton.Custom>
           {
-           ({ isConnected, show, ensName }) => {
-             // console.log("testing",isConnected);
-             if(isConnected){
-               setConnected(true);
-             }else{
-               setConnected(false);
-             }
+            ({ isConnected, show, ensName }) => {
+              if(isConnected){
+                setConnected(true);
+              }else{
+                setConnected(false);
+              }
 
-             return (
-               <div className="login" onClick={show}>
-                 {isConnected ? ensName ?? "Logout" : "Login"}
-                 <div className="bar"></div>
-               </div>
-             );
-           }
-         }
-       </ConnectKitButton.Custom>
+              return (
+                <div className="login" onClick={show}>
+                  {isConnected ? ensName ?? "Logout" : "Login"}
+                  <div className="bar"></div>
+                </div>
+              );
+            }
+          }
+        </ConnectKitButton.Custom>
           </div>
         </Container>
     )
@@ -41,45 +41,43 @@ export default Login
 
 
 const Container=styled.div`
-  width: 270px;
+  width: 300px;
   height: 50px;
+
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid black;
+
   .address-div {
     flex:2;
-    height: 100%;
-
     display: flex;
     justify-content: center;
     align-items: center;
 
     .address {
-      background-color: rgba(242, 234, 235, 0.91);
-      border-radius: 15px;
-      width: 150px;
       text-overflow: ellipsis;
       overflow: hidden;
       white-space: nowrap;
+      width: 150px;
+      background-color: lightgrey;
+      border-radius: 20px;
+      padding-left: 10px;
       padding-right: 10px;
-      padding-left: 10px;
-      padding-left: 10px;
-      padding-bottom: 3cpx;
+      paddin-top: 3px;
+      paddin-bottom: 3px;
     }
   }
 
-  .button-div {
-    margin-right: 10px;
-    margin-left: 10px;
-    border-radius: 25px;
-    width: 80px;
-    height: 36px;
-    color: white;
-    background-color: black;
+  .login-button {
+    flex:1;
     display: flex;
     justify-content: center;
     align-items: center;
+    height: 35px;
+    width: 70px;
+    background-color: black;
+    color: white;
+    border-radius: 25px;
     cursor: pointer;
 
     &:hover {
