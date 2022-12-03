9be94d9139eaed365b59cb4577af5accc90cb472
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2022-12-04 00:05:29 +0530

    log

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,38 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import { useState } from "react";
+import { ConnectKitButton } from "connectkit";
+import { useAccount } from "wagmi";
+import { Container } from "@chakra-ui/react";
 
-function Login() {
-    return (
-        <Container>
-          <div>
-            
-          </div>
-        </Container>
-    )
+export default function Login() {
+  const [isConnected, setConnected] = useState(false);
+  const { address } = useAccount();
+
+  return (
+    <Container>
+      {address}
+      <ConnectKitButton.Custom>
+        {({ isConnected, show, ensName }) => {
+          if (isConnected) {
+            setConnected(true);
+          } else {
+            setConnected(false);
+          }
+
+          return (
+            <div className="login" onClick={show}>
+              {isConnected ? ensName ?? "Logout" : "Login"}
+              <div className="bar"></div>
+            </div>
+          );
+        }}
+      </ConnectKitButton.Custom>
+    </Container>
+  );
 }
 
-export default Login
-
-
-const Container=styled.div`
+const Container = styled.div`
   width: 300px;
   height: 50px;
   border: 1px solid black;
-`
+`;
