a1896f83e524e004f9c1625b4a539b80e601c567
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2022-12-04 04:42:03 +0530

    log

--- src/pages/finalize.tsx
+++ src/pages/finalize.tsx
@@ -1,5 +1,14 @@
-import React from "react";
-import Orderform from "../components/Orderform";
+import { Container, Flex, Heading } from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { AppContext } from "../pages/_app";
+
 export default function TextTrxxack() {
-  return <Orderform />;
+  const { appState, setAppState } = useContext(AppContext);
+  return (
+    <Container>
+      <Flex>
+        <Heading>{appState}</Heading>
+      </Flex>
+    </Container>
+  );
 }
