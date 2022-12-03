6376bb9f19e7e02412ea0fe58f1eb93cdf119f66
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2022-12-03 17:44:28 +0530

    front page

--- src/pages/client.tsx
+++ src/pages/client.tsx
@@ -1,123 +1,68 @@
-import React from "react";
-import Orderform from "../components/Orderform";
-import { SimpleGrid, Flex, Image,chakra, Box, Icon } from "@chakra-ui/react";
-
-export default function TextTrxxack() {
-
-  return (
-    <SimpleGrid
-  columns={{
-    base: 1,
-    md: 2,
-  }}
-  spacing={0}
->
-  <Flex bg="brand.400">
-    <Image
-      src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"
-      alt="3 women looking at a laptop"
-      fit="cover"
-      w="full"
-      h={{
-        base: 64,
-        md: "full",
-      }}
-      bg="gray.100"
-      loading="lazy"
-      opacity={0.4}
-    />
-  </Flex>
-  <Flex
-    direction="column"
-    alignItems="start"
-    justifyContent="center"
-    px={{
-      base: 4,
-      md: 8,
-      lg: 20,
-    }}
-    py={24}
-    zIndex={3}
-  >
-    <chakra.span
-      color="brand.600"
-      _dark={{
-        color: "gray.300",
-      }}
-      fontSize="lg"
-      textTransform="uppercase"
-      fontWeight="extrabold"
-    >
-      Award winning support
-    </chakra.span>
-    <chakra.h1
-      mb={4}
-      fontSize={{
-        base: "4xl",
-        md: "4xl",
-        lg: "5xl",
-      }}
-      fontWeight="bold"
-      color="brand.600"
-      _dark={{
-        color: "gray.300",
-      }}
-      lineHeight="shorter"
-      textShadow="2px 0 currentcolor"
-    >
-      We&apos;re here to help
-    </chakra.h1>
-    <chakra.p
-      pr={{
-        base: 0,
-        lg: 16,
-      }}
-      mb={4}
-      fontSize="lg"
-      color="brand.600"
-      _dark={{
-        color: "gray.400",
-      }}
-      letterSpacing="wider"
-    >
-      Get the #1 Business Messenger and start delivering personalized
-      experiences at every stage of the customer journey.
-    </chakra.p>
-    <Box display="inline-flex" rounded="md" shadow="md">
-      <chakra.a
-        mt={2}
-        href="/link"
-        display="inline-flex"
-        alignItems="center"
-        justifyContent="center"
-        px={5}
-        py={3}
-        border="solid transparent"
-        fontWeight="bold"
-        w="full"
-        rounded="md"
-        _light={{
-          color: "white",
-        }}
-        bg="brand.600"
-        _dark={{
-          bg: "brand.500",
-        }}
-        _hover={{
-          bg: "brand.700",
-          _dark: {
-            bg: "brand.600",
-          },
-        }}
-      >
-        Visit the Help Centre
-        <Icon  ml={2} />
-      </chakra.a>
-    </Box>
-  </Flex>
-</SimpleGrid>
-  )
+import {
+    Button,
+    Flex,
+    Heading,
+    Image,
+    Stack,
+    Text,
+    useBreakpointValue,
+  } from '@chakra-ui/react';
   
-}
-
-
+  export default function SplitScreen() {
+    return (
+      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+          <Stack spacing={6} w={'full'} maxW={'lg'}>
+            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+              <Text
+                as={'span'}
+                position={'relative'}
+                _after={{
+                  content: "''",
+                  width: 'full',
+                  height: useBreakpointValue({ base: '20%', md: '30%' }),
+                  position: 'absolute',
+                  bottom: 1,
+                  left: 0,
+                  bg: 'blue.400',
+                  zIndex: -1,
+                }}>
+                Freelance
+              </Text>
+              <br />{' '}
+              <Text color={'blue.400'} as={'span'}>
+                Design Projects
+              </Text>{' '}
+            </Heading>
+            <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+              The project board is an exclusive resource for contract work. It's
+              perfect for freelancers, agencies, and moonlighters.
+            </Text>
+            <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+              <Button
+                rounded={'full'}
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                View Invoice
+              </Button>
+              <Button rounded={'full'}>
+                  Sign Off and Finalize
+              </Button>
+            </Stack>
+          </Stack>
+        </Flex>
+        <Flex flex={1}>
+          <Image
+            alt={'Login Image'}
+            objectFit={'cover'}
+            src={
+              'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+            }
+          />
+        </Flex>
+      </Stack>
+    );
+  }
