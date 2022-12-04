f412a160360c0269a23a84e795d5df7f9fc59e41
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2022-12-04 05:42:19 +0530

    comment

--- src/pages/finalize.tsx
+++ src/pages/finalize.tsx
@@ -1,6 +1,7 @@
 import { Container, Flex, Heading } from "@chakra-ui/react";
 import React, { useContext } from "react";
 import { AppContext } from "../pages/_app";
+import UpdateForm from "./updateform";
 
 export default function TextTrxxack() {
   const { appState, setAppState } = useContext(AppContext);
@@ -9,6 +10,7 @@ export default function TextTrxxack() {
       <Flex>
         <Heading>{appState}</Heading>
       </Flex>
+      <UpdateForm /> // set isDone as true after this
     </Container>
   );
 }
--- src/pages/tokenprices.tsx
+++ src/pages/tokenprices.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function () {
     return(
         <Container>
-            
+            //DUMMMY
         </Container>
     )
 }
